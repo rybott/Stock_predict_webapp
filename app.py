ca825a7193d9a5ff96c67fbb04ad66a5c069bb3c
Ryan B. <rybott5000.5@gmail.com>
2024-05-30 01:32:20 -0400

    ttt

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from Website import create_app
-from flask import render_template,session
+from alpaca.data.live import CryptoDataStream
+import time
+
+stream = CryptoDataStream("PKW7DQECIZLURTL0KOAA","tkNL2VUeS6lSVtWT6BosI4nfm0sgZw37z6HDllKj")
+
+async def handle_trade(data):
+    print(data)
+
+stream.subscribe_trades(handle_trade, "BTC/USD")
+
+stream.run()
 
 
-
-app = create_app()
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
