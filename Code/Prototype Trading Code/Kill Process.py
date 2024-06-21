8054928222bb21cc07211014083b3728cb338168
Ryan B. <rybott5000.5@gmail.com>
2024-06-20 23:38:58 -0400

    f

--- Code/Prototype Trading Code/Kill Process.py
+++ Code/Prototype Trading Code/Kill Process.py
@@ -1,11 +1,13 @@
-import asyncio
-import pandas as pd
-import time
 from alpaca.trading.client import TradingClient
 from alpaca.trading.enums import OrderType, TimeInForce, OrderSide
 from alpaca.trading.requests import MarketOrderRequest, GetOrdersRequest, QueryOrderStatus
+
+import ast
+import pandas as pd
 import os
 from dotenv import load_dotenv
+import duckdb as ddb
+
 
 load_dotenv()
 
@@ -29,3 +31,51 @@ else:
         type = OrderType.MARKET,
         time_in_force = TimeInForce.GTC)
     res = client.submit_order(req)
+
+con = ddb.connect('TradingAlgoData.duckdb')
+
+with open('data.txt', 'r') as file:
+    content = file.read()
+    Dict_list = ast.literal_eval(content)
+    df = pd.DataFrame(Dict_list)
+    df['start_timestamp'] = pd.to_datetime(df['start_timestamp'], unit='ms')
+    df['end_timestamp'] = pd.to_datetime(df['end_timestamp'], unit='ms')
+    print(df.info())
+
+# Get the Last time for the order and last entry's time in the database 
+# and if it is after then add to database FOR BOTH TABLES
+
+req = GetOrdersRequest(
+                status = QueryOrderStatus.ALL,
+                symbols = [symbol],
+                side = "buy")
+Order_df = pd.DataFrame(client.get_orders(req))
+print(Order_df.head())
+
+'''
+table = 'Price_Data'
+con.execute(
+    Add Comment 
+    CREATE TABLE IF NOT EXISTS Price_Data (
+        event_type VARCHAR,
+        pair VARCHAR,
+        open FLOAT,
+        close FLOAT,
+        high FLOAT,
+        low FLOAT,
+        volume FLOAT, 
+        vwap FLOAT,
+        start_timestamp TIMESTAMP,
+        end_timestamp TIMESTAMP,
+        avg_trade_size FLOAT
+    )
+    Add Comment
+)
+
+
+
+con.register('price_df', df)
+con.execute("INSERT INTO Price_Data SELECT * FROM price_df")
+con.close()
+
+'''
--- Code/Prototype Trading Code/Main.py
+++ Code/Prototype Trading Code/Main.py
@@ -2,8 +2,6 @@ import asyncio
 from polygon import WebSocketClient
 from polygon.websocket.models import WebSocketMessage
 from typing import List
-import pandas as pd
-import ast
 import os
 from dotenv import load_dotenv
 from TrendAnalysis import process_trade
@@ -21,15 +19,6 @@ async def handle_msg(msg: List[WebSocketMessage]):
     for m in msg:
         m_dict = m.__dict__
         await process_trade(m_dict)
-
-async def Reporting():
-    with open('data.txt', 'r') as file:
-        content = file.read()
-    Dict_list = ast.literal_eval(content)
-    df = pd.DataFrame(Dict_list)
-
-    print(df.info())
-    print(df.head())
 
 # Function to run the WebSocket client
 async def run_ws():
